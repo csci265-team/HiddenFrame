e3fec2b9dc427eb1b7513591a338bf52b33d0169
Patrick Candy <candy.pat@gmail.com>
2024-10-24 16:06:48 -0700

    adding some updates to headers

--- apps/backend/include/HiddenFrame_Headers.h
+++ apps/backend/include/HiddenFrame_Headers.h
@@ -6,21 +6,21 @@ using namespace std;
 
 class image{
     public:
-    image();
-    image(string filepath);
-    image(unsigned char* Data, long long unsigned int);
-    ~image();
-    int width;
-    int height;
-    int channels;
-    string filetype;
-    void displayImageProperties();
-    void modify_image(int n, int arr[], int arrSize);
-    string retrieve_payload(int n);
-    void write_image(string filename);
+        image();
+        image(string filepath);
+        image(unsigned char* Data, long long unsigned int);
+        ~image();
+        int width;
+        int height;
+        int channels;
+        string filetype;
+        void displayImageProperties();
+        void modify_image(int n, int arr[], int arrSize);
+        string retrieve_payload(int n);
+        void write_image(string filename);
     private:
-    void load_image(string filepath);
-    unsigned char* original_image;
-    unsigned char* modified_image;
+        void load_image(string filepath);
+        unsigned char* original_image;
+        unsigned char* modified_image;
 };
 #endif
