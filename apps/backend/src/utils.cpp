b1c9dcb458d57e26730afdb9014b41bf499d955d
payrollinsights <admin@payrollinsights.ca>
2024-11-12 16:09:49 -0800

    Draft key generation algorithms, dummy key algorithms, and key decode algorithms complete.

--- apps/backend/src/utils.cpp
+++ apps/backend/src/utils.cpp
@@ -22,3 +22,29 @@ std::string binaryToString(const std::string &binaryStr)
     }
     return text;
 }
+
+/**
+ * @brief   Converts an integer into a hex value.
+ * @param   num The integer value to be converted to hex.
+ * @return  Returns the hex value as a string.
+ */
+std::string intToHex(int num) {
+    std::stringstream ss;
+    // Set the output format to hex and use uppercase letters for A-F
+    ss << std::hex << std::uppercase << num;
+    return ss.str();
+}
+
+/**
+ * @brief   Loops through characters in a key.  Returns false if any character is not a valid hex char.
+ * @param   key The key to be checked for valid chars.
+ * @return  True if all the characters in the key are valid characters; false otherwise.
+ */
+bool isValidHexCharacter(const string& key) {
+    for (char c : key) {
+        if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+            return false;
+        }
+    }
+    return true;
+}
