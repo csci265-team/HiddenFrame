a67c314c81f28a9f443469933efcdf3e8ac24026
payrollinsights <admin@payrollinsights.ca>
2024-11-19 18:44:02 -0800

    Fix compilation errors.  Having difficulty installing on my version of macOS. Hopefully this sovles the issues Artem was having yesterday.

--- apps/backend/include/utils.h
+++ apps/backend/include/utils.h
@@ -2,3 +2,7 @@
 
 std::string stringToBinary(const std::string &str);
 std::string binaryToString(const std::string &binaryStr);
+//Converts an integer value into the corresponding Hex string.
+std::string intToHex(int num);
+//Checks whether a character is a valid Hex character.
+bool isValidHexCharacter(const std::string& key);
